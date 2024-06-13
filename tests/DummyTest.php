7374b7d2737bf08946b83f8b25bab1f694741645
Gildas Quéméner <gildas.quemener@gmail.com>
2024-06-13 17:31:43 +0200

    add cs fixer

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Kata\Tests;
 
 use PHPUnit\Framework\Attributes\Test;
@@ -10,6 +12,6 @@ final class DummyTest extends TestCase
     #[Test]
     public function is_true(): void
     {
-	self::assertTrue(true);
+        self::assertTrue(true);
     }
 }
